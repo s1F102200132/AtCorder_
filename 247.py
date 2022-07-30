1a46693ed8684f1f68616a41c488f2fb59dbff7e
s1F102200132 <s1F102200132@iniad.org>
2022-07-30 17:37:23 +0900

    yyy

--- 247.py
+++ 247.py
@@ -1,1 +1,10 @@
-A,B,K = map(int,input().split())
+def slime():
+    A,B,K = map(int,input().split())
+    num = A
+    result = 0
+    while not (num >= B):
+        result += 1
+        num *= K
+    return result
+print(slime())
+
--- 248.py
+++ 248.py
@@ -9,4 +9,3 @@ def lacked():
 
 print(lacked())
 
-        
