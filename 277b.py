427cceae43b63e6b5ec4b9052db81f1c7e789818
s1F102200132 <s1F102200132@iniad.org>
2022-12-24 18:13:57 +0900

    y

--- 277b.py
+++ 277b.py
@@ -1,15 +1,8 @@
-N = int(input())
-flag = True
-for i in range(N):
-    S = input()
-    if S[0] != 'H' or 'D' or 'C' or 'S':
-        flag = False
-    if S[1] != 'A'or '2' or '3 'or '4' or '5' or '6' or '7' or '8' or '9' or 'T' or 'J' or 'Q' or 'K':
-        flag = False
-for i in range(N-1):
-    if S[i] == S[i]:
-        flag = False
-        
-    
-print('No' if flag==False else 'Yes')
-
+import re
+pattern = re.compile(r'[HDCS][A2-9TJQK]')
+n = int(input())
+strs = [input() for i in range(n)]
+if any(not pattern.match(s) for s in strs):
+    print("No")
+else:
+    print("Yes" if len(set(strs)) == n else "No")
