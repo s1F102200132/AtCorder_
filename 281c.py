679f71ca5e97fca55179db2bfd11eb6ffc18fdd3
s1F102200132 <s1F102200132@iniad.org>
2022-12-30 15:40:00 +0900

    y

--- 281c.py
+++ 281c.py
@@ -1,11 +1,14 @@
 N,T = map(int,input().split())
 A = list(map(int,input().split()))
-number = 0
 time = 0
 
 while time <= T:
     for i in range(N):
         time += A[i]
-    break
-print(i+1,time)
+        if time >= T:
+            break
+print(i+1,time-T)
+            
+    
+
 
