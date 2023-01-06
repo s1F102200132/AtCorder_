69797884b46b61e6e624b7fd93ddb79a12a9e70f
s1F102200132 <s1F102200132@iniad.org>
2023-01-06 20:01:00 +0900

    f

--- 275.py
+++ 275.py
@@ -1,7 +1,4 @@
-n=int(input())
-h=list(map(int, input().split()))
-ans=1
-for i in range(2,len(h)+1):
-    if(h[ans-1]<h[i-1]):
-      ans=i
-print(ans)
+n = int(input())
+h = list(map(int, input().split()))
+i = h.index(max(h))
+print(i + 1)
