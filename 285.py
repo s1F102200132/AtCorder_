ab6e79fd8f7a9be05355ec7c350463ab3e6fd480
s1F102200132 <s1F102200132@iniad.org>
2023-01-15 22:45:24 +0900

    gg

--- 285.py
+++ 285.py
@@ -1,33 +1,6 @@
-a,b = map(int,input().split())
-flag = False
-if a == 1 and b == 2:
-    flag = True
-if a == 1 and b == 3:
-    flag = True
-if a == 2 and b == 4:
-    flag = True
-if a == 2 and b == 5:
-    flag = True
-if a == 4 and b == 8:
-    flag = True
-if a == 4 and b == 9:
-    flag =True
-if a == 5 and b == 10:
-    flag = True
-if a == 5 and b == 11:
-    flag = True
-if a == 3 and b == 6:
-    flag = True
-if a == 3 and b == 7:
-    flag = True
-if a == 6 and b == 12:
-    flag = True
-if a == 6 and b == 13:
-    flag = True
-if a == 7 and b == 14:
-    flag = True
-if a == 7 and b == 15:
-    flag = True
-
-
-print('Yes' if flag == True else 'No')
+a,b=map(int,input().split())
+if (a,b) in [(1,2),(1,3),(2,4),(2,5),(3,6),(3,7),\
+            (4,8),(4,9),(5,10),(5,11),(6,12),(6,13),(7,14),(7,15)]:
+  print("Yes")
+else:
+  print("No")
