940b20bffef4dff79e48dfdb4e608743349f65d2
s1F102200132 <s1F102200132@iniad.org>
2023-02-24 12:16:18 +0900

    gg

--- 289.py
+++ 289.py
@@ -1,2 +1,10 @@
 s = input()
 print(s.replace('0', 'x').replace('1', '0').replace('x', '1'))
+
+s = input()
+ 
+for i in s:
+    if i == '0':
+        print('1', end = '')
+    else:
+        print('0', end = '')
