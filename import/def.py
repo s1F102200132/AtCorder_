d3df7de2447ccd72f9a7cc534f72a0cf5e328a6f
s1F102200132 <s1F102200132@iniad.org>
2023-03-13 15:13:47 +0900

    練習

---
+++ import/def.py
@@ -0,0 +1,7 @@
+def profile(name,age,hobby):
+    print('私の名前は{}です。'.format(name))
+    print('私の年齢は{}です。'.format(age))
+    print('私の趣味は{}です。'.format(hobby))
+    
+profile('a','g','h')
+
---
+++ import/羊.py
@@ -0,0 +1,5 @@
+count = 0
+for i in range(10):
+    count += 1
+    print('ひつじが{}匹'.format(count))
+print('おやすみなさい')
